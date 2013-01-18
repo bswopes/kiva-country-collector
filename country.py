228e42bae72dcca2ae49b5a3b4b6c7ddb4d63985
Blake Swopes <bswopes@bhodisoft.com>
2013-01-17 20:01:53 -0800

    Ensure csv is closed.

--- country.py
+++ country.py
@@ -3,11 +3,12 @@
 import csv
 country_codes = {}
 
-reader = csv.reader(open('iso-country-codes.csv','r'))
-reader.next()
-for row in reader:
-        k, v = row
-        country_codes[k] = v
+with open('iso-country-codes.csv') as f:
+        reader = csv.reader(f)
+        reader.next()
+        for row in reader:
+                k, v = row
+                country_codes[k] = v
 
 #for code in country_codes:
 #        print country_codes[code]
