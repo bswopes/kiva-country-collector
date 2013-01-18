ef00c664e0b9b4abd8c51df0bfcdfb9a54c954ee
Blake Swopes <bswopes@bhodisoft.com>
2013-01-17 19:27:39 -0800

    If no new countries, will check for countries with low loan counts by user.

--- kiva-loans.py
+++ kiva-loans.py
@@ -18,6 +18,7 @@ page = 1
 baseurl = "http://api.kivaws.org/v1/lenders/" + options.kiva_id + "/loans.json?page="
 searchurl = "http://api.kivaws.org/v1/loans/search.json?status=fundraising&country_code="
 my_countries = {}
+not_loaned = country_codes.copy()
 
 url = baseurl + str(page)
 data = urllib.urlopen(url).read()
@@ -38,26 +39,34 @@ while page <= pages:
                         my_countries[code] = 1
                 else:
                         my_countries[code] += 1
+                if code in not_loaned:
+                        del not_loaned[code]
         page += 1
 
-#for code in sorted(my_countries, key=my_countries.get, reverse=True):
-#        print "Country %s, count %s" % (country_codes[str(code)],my_countries[code])
-#sys.exit(3)
 
-
-# Check for loans in countries we haven't hit yet.
-
-for code in country_codes:
-
-        for code in my_countries:
-                #print "My country:", code
-                if code in country_codes:
-                        del country_codes[code]
-
-        print "Checking country:", country_codes[code]
+def find_loans(code):
+        loans_found = False
+        #print "Checking country:", country_codes[code]
         url = searchurl + code
         data = urllib.urlopen(url).read()
         d = json.loads(data)
         loans = d["paging"]["total"]
         if loans > 0:
                 print "Found %s loans for country %s" % (loans, country_codes[code])
+                loans_found = True
+        return loans_found
+
+
+# Check for loans in countries we haven't hit yet.
+loans_found = False
+for code in not_loaned:
+        loans_found = find_loans(code)
+
+if loans_found != True:
+        print "No new countries found. Looking for less used countries."
+
+        for code in sorted(my_countries, key=my_countries.get):
+                loans_found = find_loans(code)
+                if loans_found == True:
+                        print "Country %s, previous loan count %s" % (country_codes[str(code)],my_countries[code])
+                        break;
