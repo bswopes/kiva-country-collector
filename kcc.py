aadac310818848a85bb8f4e3ffb4bede6cf23c05
Blake Swopes <bswopes@bhodisoft.com>
2013-02-23 20:41:00 -0800

    Shift lender functions to separate module.

--- kcc.py
+++ kcc.py
@@ -3,15 +3,17 @@
 import json 
 import urllib 
 import csv
-from os import mkdir, path
+from os import environ
 from sys import exit
 from optparse import OptionParser
-from webbrowser import open_new_tab
+import string
+import re
+import country
+import lender
 
 app_id = "com.bhodisoft.kcc"
 
 parser = OptionParser()
-parser.add_option("-a","--all-countries",dest="all",action="store_true",help="Check all possible countries, not just known countries Kiva loans to. Much slower.",default=False)
 parser.add_option("-d","--display-user",dest="display",action="store_true",help="Display user information only. Don't check for loans.",default=False)
 parser.add_option("-f","--find-all",dest="findall",action="store_true",help="Returns all new countries found, not just the number limited by count (-c).",default=False)
 parser.add_option("-i","--id",dest="kiva_id",type=str,help="Kiva ID from http://www.kiva.org/myLenderId")
@@ -21,136 +23,6 @@ parser.add_option("-p","--private",dest="private",action="store_true",help="Do n
 parser.add_option("-u","--update",dest="update",action="store_true",help="Ignore / Update existing user data (lender.csv)",default=False)
 parser.add_option("-v","--verbose",dest="verbose",action="store_true",help="Extra output",default=False)
 (options, args) = parser.parse_args()
-
-if options.kiva_id is None:
-        #parser.print_help()
-        lender = raw_input("Ender your Kiva ID (http://www.kiva.org/myLenderId): ")
-else:
-        lender = options.kiva_id
-
-if lender.isalnum() is False or len(lender) < 3 or len(lender) > 24:
-        print "Lender ID is invalid."
-        exit(1)
-
-lender = lender.lower()
-
-def read_countries(all):
-        country_codes = {}
-        if all:
-                if options.verbose:
-                        print "Checking all possible countries."
-                file = 'data/kiva-country-iso.csv'
-        else:
-                file = "data/kiva-country-list.csv"
-
-        with open(file) as f:
-                reader = csv.reader(f)
-                for row in reader:
-                        k, v = row
-                        country_codes[k] = v
-        return country_codes
-
-def read_lender_csv(lender):
-        ''' (str) -> dict, dict
-
-        Returns list of country codes lent to and count. Returns False if file does not exist.
-        '''
-        file = "lenders/" + lender + ".csv"
-        my_countries = {}
-        not_loaned = country_codes.copy()
-        try: 
-                with open(file,'rb') as f:
-                        loan_count = 0
-                        reader = csv.reader(f)
-                        for row in reader:
-                                key, value = row
-                                loan_count += int(value)
-                                my_countries[str(key)] = int(value)
-                                if key in not_loaned:
-                                        del not_loaned[key]
-                        if options.verbose:
-                                print "Loading cached data from file: %s" % file
-
-                        if check_lender_count(lender) != loan_count:
-                                print "Lender has made new loans. Updating..."
-                                my_countries, not_loaned = fetch_old_loans(lender)
-                        return my_countries, not_loaned
-        except IOError:
-                return False, False
-
-def write_lender_csv(lender,my_countries):
-        ''' (str,dict) -> bool
-
-        Writes country code and count to lender.csv. Returns success/failure.
-        '''
-        file = "lenders/" + lender + ".csv"
-        if not path.exists("lenders"):
-                try:
-                        mkdir("lenders",0700)
-                except IOError:
-                        if options.verbose:
-                                print "Directory already exists."
-
-                
-        try: 
-                with open(file,'wb') as f:
-                        writer = csv.writer(f,quoting=csv.QUOTE_NONNUMERIC)
-                        for key,value in my_countries.items():
-                                writer.writerow([key, value])
-                        f.close()
-                        print "Cached lender data to file: %s" % file
-                        return True
-        except IOError:
-                print "Unable to write to lender file: %s.csv" % lender
-                return False
-        
-def check_lender_count(lender):
-        lender_url = "http://api.kivaws.org/v1/lenders/" + lender + "/loans.json?app_id=" + app_id
-        try:
-                d = json.loads(urllib.urlopen(lender_url).read())
-        except:
-                print "Error loading lender page. Confirm your ID at http://www.kiva.org/myLenderId"
-                exit(1)
-        return int(d["paging"]["total"])
-
-def fetch_old_loans(lender):
-        ''' (str) -> dict,dict
-
-        Polls Kiva API for lender, gathering loan count per country.
-
-        Returns dict of country codes lent to and count. Returns dict of country codes and countries not lent to.
-
-        '''
-        page = 1 # Starting page number
-        pages = 1 # Starting limit
-        lender_url = "http://api.kivaws.org/v1/lenders/" + lender + "/loans.json?app_id=" + app_id + "&page="
-
-        my_countries = {}
-        not_loaned = country_codes.copy()
-        while page <= pages:
-                url = lender_url + str(page)
-                try:
-                        d = json.loads(urllib.urlopen(url).read())
-                except:
-                        print "Error loading lender page. Confirm your ID at http://www.kiva.org/myLenderId"
-                        exit(1)
-                pages = d["paging"]["pages"]
-
-
-                #print "Working on page %s of %s." % (page, pages)
-                for x in d["loans"]:
-                        code = x["location"]["country_code"].encode('ascii','ignore')
-                        if code not in my_countries:
-                                my_countries[code] = 1
-                        else:
-                                my_countries[code] += 1
-                        if code in not_loaned:
-                                del not_loaned[code]
-                page += 1
-
-	if not options.private:
-        	write_lender_csv(lender,my_countries)
-        return my_countries, not_loaned
 
 
 def find_loans(code):
@@ -186,23 +58,31 @@ def display_link(loans_found):
                 co_list = loans_found
 
         print "Visit Kiva at: http://www.kiva.org/lend#/?app_id=%s&countries[]=%s" % (app_id,co_list)
-        try:
-                open_new_tab("http://www.kiva.org/lend#/?app_id=%s&countries[]=%s" % (app_id,co_list))
-        except:
-                if options.verbose:
-                        print "Error opening browser."
         exit(0)
+
+#
+# Verify Lender ID has valid format
+#
+if options.kiva_id is None:
+    #parser.print_help()
+    #kiva_id = raw_input("Ender your Kiva ID (http://www.kiva.org/myLenderId): ")
+    if 'GATEWAY_INTERFACE' not in environ:
+        kiva_id = environ["USER"]
+else:
+    kiva_id = options.kiva_id
+
+lender.check_lender_id(kiva_id)
 
 #
 # Pull in data for lender's previous loans.
 #
-country_codes = read_countries(options.all)
+country_codes = country.check_kiva_countries()
 
 if options.update is False:
-        my_countries, not_loaned = read_lender_csv(lender)
+        my_countries, not_loaned = lender.read_lender_csv(kiva_id,country_codes)
 
 if options.update is True or my_countries is False:
-        my_countries, not_loaned = fetch_old_loans(lender)
+        my_countries, not_loaned = lender.fetch_old_loans(kiva_id,country_codes)
 
 
 new_list = ""
